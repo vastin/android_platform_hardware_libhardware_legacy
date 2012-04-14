eff316141e5577ffa4f9d2c6e80d038fa1e0de28
vastin <vastin@gmail.com>
2012-04-14 23:55:45 +0800

    import Mijuu patch for O2 Joggler (exclude device/o2).

--- power/Android.mk
+++ power/Android.mk
@@ -1,6 +1,10 @@
 # Copyright 2006 The Android Open Source Project
 
+ifeq ($(BOARD_DISABLE_PM),true)
+       LOCAL_SRC_FILES += power/power_disable.c
+else
 LOCAL_SRC_FILES += power/power.c
+endif
 
 ifeq ($(QEMU_HARDWARE),true)
   LOCAL_SRC_FILES += power/power_qemu.c
